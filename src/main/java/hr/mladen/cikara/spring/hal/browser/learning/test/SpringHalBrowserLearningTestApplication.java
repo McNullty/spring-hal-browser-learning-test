e42747a44f650d3b070f1fc05b3c90333dc24966
Mladen Čikara <mladen.cikara@gmail.com>
2018-09-29 12:55:07 +0200

    initial import

---
+++ src/main/java/hr/mladen/cikara/spring/hal/browser/learning/test/SpringHalBrowserLearningTestApplication.java
@@ -0,0 +1,12 @@
+package hr.mladen.cikara.spring.hal.browser.learning.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHalBrowserLearningTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringHalBrowserLearningTestApplication.class, args);
+    }
+}
