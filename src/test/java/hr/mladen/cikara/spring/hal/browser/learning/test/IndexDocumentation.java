5f61831b9ed60c49c0254b098cbb856a3ced08bb
Mladen Čikara <mladen.cikara@gmail.com>
2018-10-07 11:42:53 +0200

    Creating API Documentation for index endpoint.

---
+++ src/test/java/hr/mladen/cikara/spring/hal/browser/learning/test/IndexDocumentation.java
@@ -0,0 +1,54 @@
+package hr.mladen.cikara.spring.hal.browser.learning.test;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.restdocs.JUnitRestDocumentation;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.linkWithRel;
+import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.links;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.subsectionWithPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IndexDocumentation {
+    @Rule
+    public final JUnitRestDocumentation restDocumentation = new JUnitRestDocumentation();
+
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context)
+                .apply(documentationConfiguration(this.restDocumentation)).build();
+    }
+
+    @Test
+    public void indexExample() throws Exception {
+        this.mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andDo(document("index-example",
+                        links(
+                                linkWithRel("books").description("The <<resources-books,Books resource>>"),
+                                linkWithRel("profile").description("The ALPS profile for the service")),
+                        responseFields(
+                                subsectionWithPath("_links")
+                                        .description("<<resources-index-links,Links>> to other resources"))));
+
+    }
+}
