a48426ff7a3a6a9650aebed68523d629a87a8a9f
Mladen Čikara <mladen.cikara@gmail.com>
2018-10-20 19:03:41 +0200

    Adding test descriptor

--- src/main/java/hr/mladen/cikara/spring/hal/browser/learning/test/profile/ProfileController.java
+++ src/main/java/hr/mladen/cikara/spring/hal/browser/learning/test/profile/ProfileController.java
@@ -1,6 +1,8 @@
 package hr.mladen.cikara.spring.hal.browser.learning.test.profile;
 
-import hr.mladen.cikara.spring.hal.browser.learning.test.book.Book;
+import java.util.Arrays;
+import org.springframework.hateoas.alps.Alps;
+import org.springframework.hateoas.alps.Descriptor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,7 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProfileController {
 
   @RequestMapping(value = "books", method = RequestMethod.GET, produces = {"application/hal+json"})
-  public ResponseEntity<Book> booksProfile() {
-    return ResponseEntity.ok(null);
+  public ResponseEntity<?> booksProfile() {
+
+    Alps alps = Alps.alps().descriptors(
+            Arrays.asList(Descriptor.builder().name("test").build())).build();
+    return ResponseEntity.ok(alps);
   }
 }
