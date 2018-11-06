dcf0d8a716d5a0d0d5a83fdc3b13dbfdb52e5fb5
Mladen Čikara <mladen.cikara@gmail.com>
2018-11-06 22:30:28 +0100

    Handling wrong endpoint

--- src/main/java/hr/mladen/cikara/spring/hal/browser/learning/test/error/handling/RestExceptionHandler.java
+++ src/main/java/hr/mladen/cikara/spring/hal/browser/learning/test/error/handling/RestExceptionHandler.java
@@ -7,10 +7,10 @@ import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.HttpMediaTypeNotAcceptableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @Order(Ordered.HIGHEST_PRECEDENCE)
@@ -27,11 +27,11 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
   }
 
   @Override
-  protected ResponseEntity<Object> handleHttpMediaTypeNotAcceptable(
-          HttpMediaTypeNotAcceptableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+  protected ResponseEntity<Object> handleNoHandlerFoundException(
+          NoHandlerFoundException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
 
-    ApiError apiError = new ApiError(HttpStatus.NOT_ACCEPTABLE, ex);
-    apiError.setMessage("Test");
+    ApiError apiError = new ApiError(HttpStatus.NOT_FOUND);
+    apiError.setMessage("Resource not found " + ex.getRequestURL());
 
     return buildResponseEntity(apiError);
   }
