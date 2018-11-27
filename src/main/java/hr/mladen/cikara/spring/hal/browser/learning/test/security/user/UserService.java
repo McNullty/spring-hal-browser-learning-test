f95e12f2e6914578ec66cfa4a7c93db68826bdb2
Mladen Čikara <mladen.cikara@gmail.com>
2018-11-27 21:17:48 +0100

    Refactor interface and added documentation

--- src/main/java/hr/mladen/cikara/spring/hal/browser/learning/test/security/user/UserService.java
+++ src/main/java/hr/mladen/cikara/spring/hal/browser/learning/test/security/user/UserService.java
@@ -1,10 +1,29 @@
 package hr.mladen.cikara.spring.hal.browser.learning.test.security.user;
 
-import java.util.Collection;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface UserService {
 
+  /**
+   * Saves user.
+   *
+   * @param user User entity
+   * @return created user with id
+   */
   User save(User user);
-  Collection<User> findAll();
+
+  /**
+   * Finds all Users in repository
+   *
+   * @param pageable Pagable object
+   * @return Page with users
+   */
+  Page<User> findAll(Pageable pageable);
+
+  /**
+   * Deletes user
+   * @param id id of user to delete
+   */
   void delete(long id);
 }
