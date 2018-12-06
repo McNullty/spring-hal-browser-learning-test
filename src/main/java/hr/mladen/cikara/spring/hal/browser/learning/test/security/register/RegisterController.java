e286301e04374821cfb9e802b030dc7984f75908
Mladen Čikara <mladen.cikara@gmail.com>
2018-12-06 22:42:21 +0100

    Controller for registering users

--- src/main/java/hr/mladen/cikara/spring/hal/browser/learning/test/security/register/RegisterController.java
+++ src/main/java/hr/mladen/cikara/spring/hal/browser/learning/test/security/register/RegisterController.java
@@ -1,11 +1,13 @@
-package hr.mladen.cikara.spring.hal.browser.learning.test.register;
+package hr.mladen.cikara.spring.hal.browser.learning.test.security.register;
 
 import hr.mladen.cikara.spring.hal.browser.learning.test.security.user.UserService;
+import javax.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -18,9 +20,19 @@ public class RegisterController {
     this.userService = userService;
   }
 
+  /**
+   * Endpoint for registering new user.
+   *
+   * @param registerDto DTO with data for new user
+   * @return HTTP status NoContent
+   */
   @PostMapping(path = "/register",
           consumes = {MediaTypes.HAL_JSON_VALUE, MediaType.APPLICATION_JSON_VALUE})
-  public ResponseEntity<?> register(RegisterDto registerDto) {
-    return null;
+  public ResponseEntity<?> register(@Valid @RequestBody RegisterDto registerDto) {
+    log.debug("Got RegisterDto: {}", registerDto);
+
+    userService.register(registerDto);
+
+    return ResponseEntity.noContent().build();
   }
 }
