e5c61ea52d8e60bcaf8fc191a91849cd7d1c0222
Mladen Čikara <mladen.cikara@gmail.com>
2018-12-20 22:27:56 +0100

    WIP: Documenting authorization endpoint

---
+++ src/test/java/hr/mladen/cikara/spring/hal/browser/learning/test/documentation/AuthorizationDocumentation.java
@@ -0,0 +1,76 @@
+package hr.mladen.cikara.spring.hal.browser.learning.test.documentation;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
+import org.springframework.restdocs.payload.PayloadDocumentation;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
+import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.context.WebApplicationContext;
+
+
+@ExtendWith({RestDocumentationExtension.class})
+@SpringBootTest
+class AuthorizationDocumentation {
+
+  private MockMvc mockMvc;
+
+  /**
+   * Setting up documentation tests.
+   *
+   * @param webApplicationContext test application context
+   * @param restDocumentation documentation context provider
+   */
+  @BeforeEach
+  void setUp(final WebApplicationContext webApplicationContext,
+                    final RestDocumentationContextProvider restDocumentation) {
+    this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+            .apply(MockMvcRestDocumentation.documentationConfiguration(restDocumentation))
+            .apply(SecurityMockMvcConfigurers.springSecurity())
+            .build();
+  }
+
+  @Test
+  void authorizationExample() throws Exception {
+    MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+    params.add("grant_type", "password");
+    params.add("username", "Alex123");
+    params.add("password", "password");
+
+
+    this.mockMvc.perform(
+            MockMvcRequestBuilders.post("/oauth/token")
+                    .params(params)
+                    .with(SecurityMockMvcRequestPostProcessors.httpBasic(
+                            "application-client", "password"))
+                    .accept(MediaType.APPLICATION_JSON_VALUE))
+            .andDo(MockMvcResultHandlers.print())
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andDo(MockMvcRestDocumentation.document("authorize-example",
+//                    RequestDocumentation.pathParameters(
+//                            RequestDocumentation.parameterWithName("grant_type")
+//                                    .description("Authorization type"),
+//                            RequestDocumentation.parameterWithName("client_id")
+//                                    .description("Web Client Id"),
+//                            RequestDocumentation.parameterWithName("username")
+//                                    .description("Username"),
+//                            RequestDocumentation.parameterWithName("password")
+//                                    .description("Password")),
+                    PayloadDocumentation.responseFields(
+                            PayloadDocumentation.fieldWithPath("access_token")
+                                    .description("Access Token"))))
+    ;
+  }
+}
