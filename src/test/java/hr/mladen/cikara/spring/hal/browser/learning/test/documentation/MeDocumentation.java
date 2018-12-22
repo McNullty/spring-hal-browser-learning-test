950713bc7eeaee03ca872638a8bf1f206547c013
Mladen Čikara <mladen.cikara@gmail.com>
2018-12-22 19:05:52 +0100

    Documentation for me endpoint

---
+++ src/test/java/hr/mladen/cikara/spring/hal/browser/learning/test/documentation/MeDocumentation.java
@@ -0,0 +1,11 @@
+package hr.mladen.cikara.spring.hal.browser.learning.test.documentation;
+
+import org.junit.jupiter.api.Test;
+
+class MeDocumentation extends AbstractDocumentation {
+
+  @Test
+  void meExample() throws Exception {
+
+  }
+}
