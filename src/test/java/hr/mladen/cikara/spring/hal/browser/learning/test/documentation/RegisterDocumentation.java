8e5dd8539560639eb3d99a0736d5daf1f178471e
Mladen Čikara <mladen.cikara@gmail.com>
2019-02-19 22:04:57 +0100

    Small fix.

--- src/test/java/hr/mladen/cikara/spring/hal/browser/learning/test/documentation/RegisterDocumentation.java
+++ src/test/java/hr/mladen/cikara/spring/hal/browser/learning/test/documentation/RegisterDocumentation.java
@@ -37,7 +37,8 @@ class RegisterDocumentation extends AbstractDocumentation {
             .andDo(document("register-example",
                     requestFields(
                             fieldWithPath("username").description("Username for registration"),
-                            fieldWithPath("password").description("Password for new <<resources-users,user>>"),
+                            fieldWithPath("password")
+                                    .description("Password for new <<resources-users,user>>"),
                             fieldWithPath("passwordRepeated").description("Repeated password"))));
   }
 }
