783138ec511b3f61c68d74bcb138176d9bac2ca2
Mladen Čikara <mladen.cikara@gmail.com>
2019-02-27 20:38:08 +0100

    Enabling connection to postgres database

--- src/main/java/hr/mladen/cikara/spring/hal/browser/learning/test/security/user/User.java
+++ src/main/java/hr/mladen/cikara/spring/hal/browser/learning/test/security/user/User.java
@@ -6,10 +6,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import lombok.Builder;
 import lombok.Data;
 
 @Entity
+@Table(name = "oauth2_user")
 @Data
 public class User {
 
