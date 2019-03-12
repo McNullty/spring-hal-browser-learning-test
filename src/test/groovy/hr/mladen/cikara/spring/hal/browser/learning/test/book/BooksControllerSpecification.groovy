d61f808393b27b3243ffeff77685c401de9949ed
Mladen Čikara <mladen.cikara@gmail.com>
2019-03-12 23:38:56 +0100

    Empty specification to trigger jacoco for unit tests

---
+++ src/test/groovy/hr/mladen/cikara/spring/hal/browser/learning/test/book/BooksControllerSpecification.groovy
@@ -0,0 +1,12 @@
+package hr.mladen.cikara.spring.hal.browser.learning.test.book
+
+import spock.lang.Specification
+
+class BooksControllerSpecification extends Specification {
+    // TODO: Add tests
+    def "findAll should return list of books"() {
+        given: "Book controller with mocked dependencies"
+        when: "calling findAll"
+        then: "list of books is returned"
+    }
+}
