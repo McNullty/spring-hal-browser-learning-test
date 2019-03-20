d5229a3fdbb3d3458135e4cfc9ed91d4da7385d2
Mladen Čikara <mladen.cikara@gmail.com>
2019-03-20 23:10:51 +0100

    Endpoint for changing user password without business logic

--- src/main/java/hr/mladen/cikara/spring/hal/browser/learning/test/security/user/UserController.java
+++ src/main/java/hr/mladen/cikara/spring/hal/browser/learning/test/security/user/UserController.java
@@ -17,6 +17,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -37,8 +38,8 @@ public class UserController {
    * @param userToUserResourceAssembler Assembler to convert form Zser to UserResource
    */
   public UserController(
-          final UserService userService,
-          final UserToUserResourceAssembler userToUserResourceAssembler) {
+      final UserService userService,
+      final UserToUserResourceAssembler userToUserResourceAssembler) {
     Assert.notNull(userService, "Controller can't work without user service");
     Assert.notNull(userToUserResourceAssembler, "Controller can't work without resource assembler");
 
@@ -55,8 +56,8 @@ public class UserController {
    */
   @GetMapping(produces = {MediaTypes.HAL_JSON_VALUE, MediaType.APPLICATION_JSON_VALUE})
   public ResponseEntity<PagedResources<UserResource>> findAll(
-          final Pageable pageable,
-          final PagedResourcesAssembler<User> assembler) {
+      final Pageable pageable,
+      final PagedResourcesAssembler<User> assembler) {
 
     Page<User> users = userService.findAll(pageable);
 
@@ -67,7 +68,7 @@ public class UserController {
   }
 
   private PagedResources<UserResource> getPagedUserResourcesWithLinks(
-          final PagedResourcesAssembler<User> assembler, final Page<User> users) {
+      final PagedResourcesAssembler<User> assembler, final Page<User> users) {
     Link self = ControllerLinkBuilder.linkTo(UserController.class).withSelfRel();
 
     return assembler.toResource(users, userToUserResourceAssembler, self);
@@ -81,10 +82,10 @@ public class UserController {
    * @throws UserService.UserNotFoundException when user with give ID is not found
    */
   @GetMapping(
-          value = "/{userId}",
-          produces = {MediaTypes.HAL_JSON_VALUE, MediaType.APPLICATION_JSON_VALUE})
+      value = "/{userId}",
+      produces = {MediaTypes.HAL_JSON_VALUE, MediaType.APPLICATION_JSON_VALUE})
   public ResponseEntity<UserResource> getUser(@PathVariable final Long userId)
-          throws UserService.UserNotFoundException {
+      throws UserService.UserNotFoundException {
     User user = userService.findById(userId);
 
     UserResource userResource = userToUserResourceAssembler.toResource(user);
@@ -99,8 +100,8 @@ public class UserController {
    * @return UserResource
    */
   @GetMapping(
-          value = "/me",
-          produces = {MediaTypes.HAL_JSON_VALUE, MediaType.APPLICATION_JSON_VALUE})
+      value = "/me",
+      produces = {MediaTypes.HAL_JSON_VALUE, MediaType.APPLICATION_JSON_VALUE})
   public ResponseEntity<UserResource> getCurrentUser(Principal principal) {
     log.debug("Principal: {}", principal);
 
@@ -115,5 +116,12 @@ public class UserController {
 
       throw new RuntimeException("Current user not foind!");
     }
+  }
+
+  @PostMapping(
+      value = "/{userId}/change-password",
+      consumes = {MediaTypes.HAL_JSON_VALUE, MediaType.APPLICATION_JSON_VALUE})
+  public ResponseEntity<?> changePassword(@PathVariable final Long userId) {
+    return ResponseEntity.accepted().build();
   }
 }
