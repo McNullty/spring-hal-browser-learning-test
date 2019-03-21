06cedf34f1cf094a428275353f663e0eff18abad
Mladen Čikara <mladen.cikara@gmail.com>
2019-03-21 20:55:38 +0100

    Test for changing password

--- src/integration-test/groovy/hr/mladen/cikara/spring/hal/browser/learning/test/security/user/UserControllerSpecification.groovy
+++ src/integration-test/groovy/hr/mladen/cikara/spring/hal/browser/learning/test/security/user/UserControllerSpecification.groovy
@@ -1,8 +1,10 @@
 package hr.mladen.cikara.spring.hal.browser.learning.test.security.user
 
-
+import com.fasterxml.jackson.databind.ObjectMapper
+import groovy.util.logging.Slf4j
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.json.JacksonJsonParser
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
 import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders
@@ -22,33 +24,28 @@ import spock.lang.Unroll
 /**
  * Integration test for UserController.
  */
+@Slf4j
 @Unroll
 @SpringBootTest
+@AutoConfigureMockMvc
 class UserControllerSpecification extends Specification {
 
+    public static final String TEST_USER = "Alex123"
+    @Autowired
     MockMvc mockMvc
 
-//    @Autowired
-//    ObjectMapper objectMapper
+    @Autowired
+    UserRepository userRepository
 
     @Autowired
-    WebApplicationContext webApplicationContext
-
-//    @Autowired
-//    UserRepository userRepository
-
-    void setup() {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext)
-                .apply(SecurityMockMvcConfigurers.springSecurity())
-                .build()
-    }
+    private ObjectMapper objectMapper
 
     def 'find all users endpoint (users/)'() {
         when: 'you perform get operation'
         def result = mockMvc.perform(
                 RestDocumentationRequestBuilders.get("/users/")
                         .header("Authorization", "Bearer " + getAuthorizationResponse())
-                        .accept(MediaType.APPLICATION_JSON_VALUE))
+                        .contentType(MediaType.APPLICATION_JSON))
                 .andDo(MockMvcResultHandlers.print())
 
         then: 'expect to get status OK'
@@ -66,6 +63,45 @@ class UserControllerSpecification extends Specification {
         and: 'user resource is returned'
     }
 
+    def 'changing password for user'() {
+        given: 'valid authorization token'
+        def authorization = getAuthorizationResponse()
+        log.debug("Authorization: {}", authorization)
+
+        and: 'test user from repository'
+        def testUser =  userRepository.findByUsername(TEST_USER).get()
+
+        and: 'user id for test user'
+        def testUserId = testUser.getId()
+        log.debug("User ID: {}", testUserId)
+
+        and: 'old password is fetched from repository'
+        def oldPassword = testUser.getPassword()
+        log.debug("Old password: {}", oldPassword)
+
+        and: 'request body with same password and repeated password'
+        def requestBody = new HashMap<String, Object>()
+        requestBody.put("password", "newPassword")
+        requestBody.put("passwordRepeated", "newPassword")
+
+        when: '/change-password endpoint is called'
+        def result = mockMvc.perform(
+                RestDocumentationRequestBuilders.put("/users/" + testUserId + "/change-password")
+                        .header("Authorization", "Bearer " + getAuthorizationResponse())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(requestBody)))
+                .andDo(MockMvcResultHandlers.print())
+
+        then: 'ACCEPTED is returned'
+        result.andExpect(MockMvcResultMatchers.status().isNoContent())
+
+        and: 'new password is saved in repository'
+        def newTestUser =  userRepository.findByUsername(TEST_USER).get()
+        log.debug("New password: {}", newTestUser.getPassword())
+
+        newTestUser.getPassword() != oldPassword
+    }
+
     /**
      * Sends authorization data and returns response from oauth2 server.
      *
@@ -75,7 +111,7 @@ class UserControllerSpecification extends Specification {
     String getAuthorizationResponse() throws Exception {
         MultiValueMap<String, String> loginParams = new LinkedMultiValueMap<>()
         loginParams.add("grant_type", "password")
-        loginParams.add("username", "Alex123")
+        loginParams.add("username", TEST_USER)
         loginParams.add("password", "password")
 
         String resultString = this.mockMvc.perform(
