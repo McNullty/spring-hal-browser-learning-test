fd8d18cbea3ff1a1085513dd24d95ca264f2d99c
Mladen Čikara <mladen.cikara@gmail.com>
2019-06-06 11:02:22 +0200

    Small fix

--- src/main/java/hr/mladen/cikara/spring/hal/browser/learning/test/security/configuration/JwtProperties.java
+++ src/main/java/hr/mladen/cikara/spring/hal/browser/learning/test/security/configuration/JwtProperties.java
@@ -1,10 +1,11 @@
 package hr.mladen.cikara.spring.hal.browser.learning.test.security.configuration;
 
 import lombok.Data;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
 
-@Component
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 @ConfigurationProperties(prefix = "custom.jwt")
 @Data
 public class JwtProperties {
